24d547e48261d53cca75b5a99b3172b52446f5e7
SHShadhin <shshadhin0@gmail.com>
2026-03-29 19:45:41 +0600

    React Project Setup, Tailwind CSS and Daisyui added done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl text-center text-red-400'>React Project Setup, Tailwind and Daisyui added done</h2>
+      
+    </>
+  )
+}
+
+export default App
