f200c40542171d885643faccb7e5038d97c4500c
SHShadhin <shshadhin0@gmail.com>
2026-03-30 09:31:07 +0600

    Fetch jason data and show tools card dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
+import Tools from './components/main/Tools'
 import NavBar from './components/Navbar'
 import Price from './components/Price'
 import Stats from './components/Stats'
 import Steps from './components/Steps'
 import Transform from './components/Transform'
+
+const getTools = async () => {
+  const res = await fetch('/tools.json');
+  return res.json();
+}
+const toolsPromise = getTools();
 
 function App() {
 
@@ -15,6 +22,8 @@ function App() {
       <Banner></Banner>
       <Stats></Stats>
 
+
+      <Tools toolsPromise={toolsPromise}></Tools>
 
       <Steps></Steps>
       <Price></Price>
